6cc0a99e147d546da43820c7645c156a21b8d64f
Chris Mueller <christoph.mueller@dfki.de>
2012-03-30 14:34:45 +0200

    add code for visualizing world limits.

--- viz/LandmarkVisualization.cpp
+++ viz/LandmarkVisualization.cpp
@@ -7,6 +7,7 @@ namespace vizkit {
 LandmarkVisualization::LandmarkVisualization()
 {
     VizPluginRubyAdapter(LandmarkVisualization, uw_localization::LandmarkMap, Map);
+    changed = true;
 }
 
 
@@ -43,36 +44,59 @@ osg::ref_ptr<osg::Node> LandmarkVisualization::createMainNode() {
 
 void LandmarkVisualization::updateMainNode(osg::Node* node )
 {
-    osg::Group* group = node->asGroup();
-    osg::Group* world = group->getChild(0)->asGroup();
-    osg::Group* landmarks = group->getChild(1)->asGroup();
+    if(changed) {
+        osg::Group* group = node->asGroup();
+        osg::Group* landmarks = group->getChild(1)->asGroup();
 
-    // visualize landmarks
-    while(map.landmarks.size() != landmarks->getNumChildren()) {
-        if(map.landmarks.size() < landmarks->getNumChildren())
-            landmarks->removeChildren(0, landmarks->getNumChildren() - map.landmarks.size());
+        // visualize landmarks
+        while(map.landmarks.size() != landmarks->getNumChildren()) {
+            if(map.landmarks.size() < landmarks->getNumChildren())
+                landmarks->removeChildren(0, landmarks->getNumChildren() - map.landmarks.size());
 
-        if(map.landmarks.size() > landmarks->getNumChildren())
-            group->addChild(new Uncertainty);
+            if(map.landmarks.size() > landmarks->getNumChildren())
+                group->addChild(new Uncertainty);
+        }
+
+        for(unsigned i = 0; i < map.landmarks.size(); i++) {
+            Uncertainty* uncertainty = dynamic_cast<Uncertainty*>(landmarks->getChild(i));  
+            uncertainty->hideSamples();
+            uncertainty->setMean(static_cast<Eigen::Vector3d>(map.landmarks[i].mean));
+            uncertainty->setCovariance(static_cast<Eigen::Matrix3d>(map.landmarks[i].covariance));
+        }
+
+        vertices->clear();
+
+        // visualize world limitations#
+        Eigen::Translation3d tr = Eigen::Translation3d(map.translation);
+        Eigen::Vector3d lt = map.limitations;
+
+        vertices->push_back(osg::Vec3d(tr.x(), tr.y(), tr.z()));
+        vertices->push_back(osg::Vec3d(tr.x() + lt.x(), tr.y(), tr.z()));
+        vertices->push_back(osg::Vec3d(tr.x() + lt.x(), tr.y(), tr.z() + lt.z()));
+        vertices->push_back(osg::Vec3d(tr.x(), tr.y(), tr.z() + lt.z()));
+
+        geom->setVertexArray(vertices);
+        geom->removePrimitiveSet(0, geom->getNumPrimitiveSets());
+
+        for(size_t i = 0; i < (vertices->size() - 3); i += 4) {
+            osg::ref_ptr<osg::DrawElementsUInt> draw = new osg::DrawElementsUInt(osg::PrimitiveSet::QUADS, 0);
+
+            draw->push_back(i);
+            draw->push_back(i + 1);
+            draw->push_back(i + 2);
+            draw->push_back(i + 3);
+            geom->addPrimitiveSet(draw);
+        }
+
+        changed = false;
     }
-
-    for(unsigned i = 0; i < map.landmarks.size(); i++) {
-        Uncertainty* uncertainty = dynamic_cast<Uncertainty*>(landmarks->getChild(i));  
-        uncertainty->hideSamples();
-        uncertainty->setMean(static_cast<Eigen::Vector3d>(map.landmarks[i].mean));
-        uncertainty->setCovariance(static_cast<Eigen::Matrix3d>(map.landmarks[i].covariance));
-    }
-
-    // visualize world limitations
-    vertices->clear();
-
-
 }
 
 void
 LandmarkVisualization::updateDataIntern(uw_localization::LandmarkMap const& value)
 {
     map = value;
+    changed = true;
 }
  
 VizkitQtPlugin(LandmarkVisualization)
--- viz/LandmarkVisualization.hpp
+++ viz/LandmarkVisualization.hpp
@@ -27,6 +27,7 @@ private:
     osg::ref_ptr<osg::Geometry> geom;
     osg::ref_ptr<osg::Vec3Array> vertices;
     osg::ref_ptr<osg::Vec4Array> colors;
+    bool changed;
 };
 }
 
